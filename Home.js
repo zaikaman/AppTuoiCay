fb99aba219d0e9d94ac89d1e2dcb70deedb449a6
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-01-17 00:08:42 +0700

    Update Home.js
    - Đã chạy được video background cây
    - Nhớ chạy lệnh npm install expo-av trước

--- Home.js
+++ Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import VLCPlayer from 'react-native-vlc-media-player';
+import { Video } from 'expo-av';
 
 const Home = () => {
   return (
-    <VLCPlayer
+    <Video
+      source={require('../assets/videos/video.mp4')}
+      rate={1.0}
+      volume={1.0}
+      isMuted={false}
+      resizeMode="cover"
+      shouldPlay
+      isLooping
       style={{ width: '100%', height: '100%' }}
-      video={{ uri: '../assets/videos/video.mp4' }}
-      autoplay={true}
-      isLooping={true}
     />
   );
 };
