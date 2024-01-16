67f13e06ec6baeb9a12c66e7f28edab4e7c888a0
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-01-17 00:00:19 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { FONTS } from './constants/fonts';
+import { useCallback, useEffect } from 'react';
+import { Login, Signup, Welcome, Home } from './screens';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts(FONTS);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  useEffect(() => {
+    onLayoutRootView();
+  }, [onLayoutRootView]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Welcome'>
+            <Stack.Screen name='Welcome' component={Welcome} />
+            <Stack.Screen name='Login' component={Login} />
+            <Stack.Screen name='Signup' component={Signup} />
+            <Stack.Screen name='Home' component={Home} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
+  );
+}
