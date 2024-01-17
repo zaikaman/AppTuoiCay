962de2c8ef42ed56b5d9a4ea448b8c8e4ae0344c
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-01-17 23:22:17 +0700

    Update index.js
    đã quá

--- index.js
+++ index.js
@@ -2,20 +2,11 @@ import Login from "./Login";
 import Signup from "./Signup";
 import Welcome from "./Welcome";
 import Home from "./Home";
-import User from "./User";
-import Setting from "./SettingScreen";
-import Shop from "./Shop";
-import Vip from "./Vip";
-import Leaderboard from "./Leaderboard";
+export { default as Leaderboard } from './Leaderboard';
 
 export {
     Login,
     Signup,
     Welcome,
-    Home,
-    User,
-    Leaderboard,
-    Vip,
-    Shop,
-    Setting,
+    Home
 }
