bc66b06375e87952ff1b824b693c0b5e67844342
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-01-18 13:14:29 +0700

    Update App.js
    - Sửa một số thông báo lỗi

--- App.js
+++ App.js
@@ -5,10 +5,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { FONTS } from './constants/fonts';
 import { useCallback, useEffect } from 'react';
-import { Login, Signup, Welcome, Home } from './screens';
+import { Login, Signup, Welcome, Home, User, Vip, Shop, Leaderboard, Setting} from './screens';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { Leaderboard } from './screens';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Animated: `useNativeDriver` was not specified.']);
+LogBox.ignoreLogs(['ViewPropTypes has been removed from React Native.']);
+LogBox.ignoreLogs(['componentWillReceiveProps has been renamed, and is not recommended for use.']);
+LogBox.ignoreLogs(['The method or property expo-font.unloadAsync is not available on android, are you sure you\'ve linked all the native dependencies properly?']);
 
 const Stack = createNativeStackNavigator();
 
@@ -38,6 +43,10 @@ export default function App() {
             <Stack.Screen name='Login' component={Login} />
             <Stack.Screen name='Signup' component={Signup} />
             <Stack.Screen name='Home' component={Home} />
+            <Stack.Screen name='User' component={User} />
+            <Stack.Screen name='Shop' component={Shop} />
+            <Stack.Screen name='Vip' component={Vip} />
+            <Stack.Screen name='Setting' component={Setting}  />
             <Stack.Screen name='Leaderboard' component={Leaderboard} />
           </Stack.Navigator>
         </NavigationContainer>
@@ -45,4 +54,3 @@ export default function App() {
     </Provider>
   );
 }
-
