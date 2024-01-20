50d5616073e4c0ae35f31fabbf18153094a85da5
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-01-20 22:47:50 +0700

    Update App.js
    - Thêm gestureroot gì đó hong bit

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React, { useState, useCallback, useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { FONTS } from './constants/fonts';
-import { Login, Signup, Welcome, Home, User, Vip, Shop, Leaderboard, Setting} from './screens';
+import { useCallback, useEffect } from 'react';
+import { Login, Signup, Welcome, Home, User, Vip, Shop, Leaderboard, Setting, Spin} from './screens';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { LogBox } from 'react-native';
-import VolumeContext from './VolumeContext'; // Đảm bảo rằng bạn đã tạo file này
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+LogBox.ignoreLogs(['Warning: Failed prop type: Invalid prop `source` supplied to `Image`, expected one of type [number].']);
 LogBox.ignoreLogs(['Animated: `useNativeDriver` was not specified.']);
 LogBox.ignoreLogs(['ViewPropTypes has been removed from React Native.']);
 LogBox.ignoreLogs(['componentWillReceiveProps has been renamed, and is not recommended for use.']);
@@ -20,7 +21,6 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts(FONTS);
-  const [volume, setVolume] = useState(0.5); // Trạng thái âm lượng mới
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -39,7 +39,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <VolumeContext.Provider value={{ volume, setVolume }}> {/* Thêm VolumeContext.Provider */}
+        <GestureHandlerRootView style={{flex: 1}}>
           <NavigationContainer>
             <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Welcome'>
               <Stack.Screen name='Welcome' component={Welcome} />
@@ -49,11 +49,12 @@ export default function App() {
               <Stack.Screen name='User' component={User} />
               <Stack.Screen name='Shop' component={Shop} />
               <Stack.Screen name='Vip' component={Vip} />
-              <Stack.Screen name='Setting' component={Setting}  />
+              <Stack.Screen name='Setting' component={Setting} />
+              <Stack.Screen name='Spin' component={Spin} />
               <Stack.Screen name='Leaderboard' component={Leaderboard} />
             </Stack.Navigator>
           </NavigationContainer>
-        </VolumeContext.Provider> {/* Kết thúc VolumeContext.Provider */}
+        </GestureHandlerRootView>
       </SafeAreaProvider>
     </Provider>
   );
