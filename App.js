c5d3fbbc90469706c41b965201fa290850dc2d4b
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-01-21 23:02:29 +0700

    Update App.js

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { FONTS } from './constants/fonts';
-import { useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react'; // Add useState here
 import { Login, Signup, Welcome, Home, User, Vip, Shop, Leaderboard, Setting, Spin} from './screens';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { LogBox } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { ShopContext } from './screens/ShopContext';
 
 LogBox.ignoreLogs(['Warning: Failed prop type: Invalid prop `source` supplied to `Image`, expected one of type [number].']);
 LogBox.ignoreLogs(['Animated: `useNativeDriver` was not specified.']);
@@ -20,6 +21,7 @@ LogBox.ignoreLogs(['The method or property expo-font.unloadAsync is not availabl
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [selectedItem, setSelectedItem] = useState(null);
   const [fontsLoaded] = useFonts(FONTS);
 
   const onLayoutRootView = useCallback(async () => {
@@ -41,18 +43,20 @@ export default function App() {
       <SafeAreaProvider>
         <GestureHandlerRootView style={{flex: 1}}>
           <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Welcome'>
-              <Stack.Screen name='Welcome' component={Welcome} />
-              <Stack.Screen name='Login' component={Login} />
-              <Stack.Screen name='Signup' component={Signup} />
-              <Stack.Screen name='Home' component={Home} />
-              <Stack.Screen name='User' component={User} />
-              <Stack.Screen name='Shop' component={Shop} />
-              <Stack.Screen name='Vip' component={Vip} />
-              <Stack.Screen name='Setting' component={Setting} />
-              <Stack.Screen name='Spin' component={Spin} />
-              <Stack.Screen name='Leaderboard' component={Leaderboard} />
-            </Stack.Navigator>
+            <ShopContext.Provider value={{ selectedItem, setSelectedItem }}> 
+              <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Welcome'>
+                <Stack.Screen name='Welcome' component={Welcome} />
+                <Stack.Screen name='Login' component={Login} />
+                <Stack.Screen name='Signup' component={Signup} />
+                <Stack.Screen name='Home' component={Home} />
+                <Stack.Screen name='User' component={User} />
+                <Stack.Screen name='Shop' component={Shop} />
+                <Stack.Screen name='Vip' component={Vip} />
+                <Stack.Screen name='Setting' component={Setting} />
+                <Stack.Screen name='Spin' component={Spin} />
+                <Stack.Screen name='Leaderboard' component={Leaderboard} />
+              </Stack.Navigator>
+            </ShopContext.Provider>
           </NavigationContainer>
         </GestureHandlerRootView>
       </SafeAreaProvider>
